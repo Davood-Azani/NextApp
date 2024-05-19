62a797b181602c0cea0dbafc4640cf33be8983e1
tt005067 <tt005067@gmail.com>
2024-05-19 23:34:09 +0330

    Clean app

--- .notes/n1.js
+++ .notes/n1.js
@@ -1,1 +1,7 @@
 //# Instal by  : npx create-next-app@14 project-name
+//----->
+//# Clean Project
+//? Edit page.js and clean it
+//# Run : npm run dev
+//? we can see the src of page and we can see the code of hello next, means it comes from server
+//----->

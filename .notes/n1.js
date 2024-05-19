b5247cf8fa26cb10526e6cfde10cbbcebfc31017
tt005067 <tt005067@gmail.com>
2024-05-19 23:55:26 +0330

    Route

--- .notes/n1.js
+++ .notes/n1.js
@@ -4,4 +4,8 @@
 //? Edit page.js and clean it
 //# Run : npm run dev
 //? we can see the src of page and we can see the code of hello next, means it comes from server
+//----->Route
+//? Each folder in App is Segment and into it we need a component called page.js
+//# it's server Comp
+//? the page in the route in homePage
 //----->

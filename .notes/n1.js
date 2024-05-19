c6957961863f0e84625b10e510003c1c111f44b5
tt005067 <tt005067@gmail.com>
2024-05-20 00:40:06 +0330

    RootLayout&Logo

--- .notes/n1.js
+++ .notes/n1.js
@@ -12,4 +12,11 @@
 //# a tag : using it navigate us to the target page but with full reloading
 //# we use Link (next) :  <Link href="/cabins">Cabins</Link>
 //? we make a folder called components in app and we make a component called navigation and we put our link in there,
+//----->Layout and GlobalLayout or rootLayout
+//? make a file called layout in the root of app folder
+//# this name is a convention name in next like page , we can write the component name as what we want here we Write RootLayout, take children to display in body(outlet was in react)
+//# we make the main layout in the RootLayout with main footer header and body and html.
+//| we can put some meta data in the layout, and as if it a server component we can see it in the source
+//| we make a conponent called logo to display logo
+//? we put the logo in the public folder and we have access with a slash
 //----->
--- app/page.js
+++ app/page.js
@@ -4,10 +4,7 @@ import Navigation from './components/Navigation';
 export default function Home() {
   return (
     <div>
-      <Navigation />
       <h1>Home Next!</h1>
-
-      <Link href="/cabins">Cabins</Link>
     </div>
   );
 }

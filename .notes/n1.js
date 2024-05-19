f4cb8fee7a47ad7ec0aa196bc264c5c1a8e10d47
tt005067 <tt005067@gmail.com>
2024-05-20 00:09:59 +0330

    Navigation & Link

--- .notes/n1.js
+++ .notes/n1.js
@@ -8,4 +8,8 @@
 //? Each folder in App is Segment and into it we need a component called page.js
 //# it's server Comp
 //? the page in the route in homePage
+//----->Navigate Between Page
+//# a tag : using it navigate us to the target page but with full reloading
+//# we use Link (next) :  <Link href="/cabins">Cabins</Link>
+//? we make a folder called components in app and we make a component called navigation and we put our link in there,
 //----->
--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+import Navigation from './components/Navigation';
+
 export default function Home() {
   return (
     <div>
+      <Navigation />
       <h1>Home Next!</h1>
+
+      <Link href="/cabins">Cabins</Link>
     </div>
   );
 }

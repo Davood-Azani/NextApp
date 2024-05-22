e2809d41b62d0087cba46b08a529f75ad160ee57
tt005067 <tt005067@gmail.com>
2024-05-22 18:35:41 +0330

    Server and Client Boundary and Tips

---
+++ .notes/n4.js
@@ -0,0 +1,19 @@
+//----->BLURRING THE BOUNDARY BETWEEN SERVER AND CLIENT
+//? Client Components :
+//# Can import : Only Client Components
+//# Can render : Client and Server Components passed as Props
+
+//? Server Components
+//# Can import : Client and Server Components
+//# Can render : Client and Server Components
+
+//! we can render  A Server Component inside a Client Component with passing as props (children , props)
+
+//? ex: in A we want Client Component  B and Server Component A
+//? first in A, Component B and D must be imported then in B as children must be D
+//!  children of client components will automatically become client components as well.
+//# it's Possible a component in a place is Server Component and in other place is Client Component : for this to make sense, it's very important to understand that components actually create instances when they are used and that therefore the type of component instance that will be created is not set in stone when we write the component code.
+//* If the component is imported and used by a client component,so inside the client server boundary,it'll end up creating an instance of a client component too. Otherwise an instance of a server component is created instead.
+
+
+//----->CLIENT COMPONENTS IN SERVER COMPONENTS
